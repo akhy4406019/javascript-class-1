ee926bc96b3b0f4fab780425e16c5a9de7130a44
akhy4406019 <149380498+akhy4406019@users.noreply.github.com>
2024-01-15 11:56:58 +0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,39 @@
+// document.getElementById('head').innerHTML='My name is Akhy';
+
+
+// var x=10;
+// var y=33;
+// document.write(x+y);
+// window.alert(x-y);
+// console.log(x*y);
+
+
+// var id=4406019;
+// id=4405877;
+// console.log(id);
+
+
+//  var id,sname,classname;
+//  id=4406019;
+//  sname=' Akhy ';
+//  classname=' Eight ';
+//  document.write(sname+classname+id);
+
+
+// let x=99;
+// let y=62;
+// document.write(x+y);
+
+
+document.getElementById('demo').style.color='purple';
+document.getElementById('demo').style.fontSize='28px';
+document.getElementById('demo').style.border='2px solid purple';
+document.getElementById('demo').style.borderRadius='5px';
+
+
+document.getElementsByClassName('para')[0].style.color='red';
+document.getElementsByClassName('para')[0].style.fontSize='28px'
+document.getElementsByClassName('para')[0].style.border='2px solid red';
+document.getElementsByClassName('para')[0].style.borderRadius='5px';
+
+
